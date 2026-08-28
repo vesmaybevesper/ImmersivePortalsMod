e7851a2e290de5a8818a255ffa2000d04365fc6e
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-08-28 18:15:29 -0400

    port cca fixed compat

--- src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
+++ src/main/java/qouteall/imm_ptl/core/compat/mixin/cardinal_comp/MixinCardinalCompComponentKey.java
@@ -2,36 +2,35 @@ package qouteall.imm_ptl.core.compat.mixin.cardinal_comp;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.Nullable;
+import org.ladysnake.cca.api.v3.component.ComponentKey;
+import org.ladysnake.cca.api.v3.component.ComponentProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Coerce;
 import qouteall.imm_ptl.core.network.PacketRedirection;
 
-@Pseudo
-@Mixin(targets = "org.ladysnake.cca.api.v3.component.ComponentKey")
+@Mixin(ComponentKey.class)
 public class MixinCardinalCompComponentKey {
     // redirect the entity sync packet
     @SuppressWarnings({"unchecked", "rawtypes"})
     @WrapOperation(
-        method = "Ldev/ladysnake/cca/api/v3/component/ComponentKey;syncWith(Lnet/minecraft/server/level/ServerPlayer;Ldev/ladysnake/cca/api/v3/component/ComponentProvider;Ldev/ladysnake/cca/api/v3/component/sync/ComponentPacketWriter;Ldev/ladysnake/cca/api/v3/component/sync/PlayerSyncPredicate;)V",
+        method = "syncWith(Lnet/minecraft/server/level/ServerPlayer;Lorg/ladysnake/cca/api/v3/component/ComponentProvider;Lorg/ladysnake/cca/api/v3/component/sync/ComponentPacketWriter;Lorg/ladysnake/cca/api/v3/component/sync/PlayerSyncPredicate;)V",
         at = @At(
             value = "INVOKE",
-            target = "Ldev/ladysnake/cca/api/v3/component/ComponentProvider;toComponentPacket(Ldev/ladysnake/cca/api/v3/component/ComponentKey;Ldev/ladysnake/cca/api/v3/component/sync/ComponentPacketWriter;Lnet/minecraft/server/level/ServerPlayer;)Lnet/minecraft/network/protocol/common/ClientboundCustomPayloadPacket;"
+            target = "Lorg/ladysnake/cca/api/v3/component/ComponentProvider;toComponentPacket(Lorg/ladysnake/cca/api/v3/component/ComponentKey;ZLnet/minecraft/network/RegistryFriendlyByteBuf;)Lnet/minecraft/network/protocol/common/custom/CustomPacketPayload;"
         )
     )
-    private ClientboundCustomPayloadPacket redirectPacket(
-        @Coerce Object instance, @Coerce Object key,
-        @Coerce Object writer, ServerPlayer recipient,
-        Operation<Packet<?>> operation
+    private @Nullable CustomPacketPayload redirectPacket(
+            ComponentProvider instance, ComponentKey key, boolean required, RegistryFriendlyByteBuf data, Operation<CustomPacketPayload> original
     ) {
-        Packet<?> packet = operation.call(instance, key, writer, recipient);
+        Packet<?> packet = (Packet<?>) original.call(instance, key, required, data);
         
         if (instance instanceof Entity entity) {
             var redirected = PacketRedirection.createRedirectedMessage(
@@ -50,6 +49,6 @@ public class MixinCardinalCompComponentKey {
             packet = redirected;
         }
         
-        return (ClientboundCustomPayloadPacket) packet;
+        return ((ClientboundCustomPayloadPacket) packet).payload();
     }
 }
